b2d96a3571f2bb6604fd2e920c621708ec0bb583
Ярослав Розлуцкий <papa7717@mail.ru>
2022-07-17 22:52:25 +0300

    Добавил 'Выполненую' задачу Func

--- 2ndHW/Multiplication.py
+++ 2ndHW/Multiplication.py
@@ -3,7 +3,6 @@
 def Multiplicate(arg):
     if type(arg) != int and type(arg) != float:
         print("Invalid data")
-    i = 0
     Mult = 1
     for i in range(1, arg + 1):
         Mult = Mult * i
