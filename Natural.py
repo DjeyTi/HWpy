ed0e3061cf9c812297717594b7ef4747302f7fa3
Ярослав Розлуцкий <papa7717@mail.ru>
2022-07-25 08:19:28 +0300

    Снова добавил файл Natural

---
+++ Natural.py
@@ -0,0 +1,13 @@
+# Задайте натуральное число N. Напишите программу, которая составит список простых множителей числа N.
+
+import random as rnd
+
+def Natural(arg):
+    list = []
+    for i in range(2, arg + 1):
+        if arg % i == 0:
+            list.append(i)
+    print(F"Added argument {arg} have next numbers, that gives 0 in process of division: {list}")
+
+x = rnd.randint(2, 100)
+Natural(x)
