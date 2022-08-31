f3ef103a79999fb922a6ce2c24ef3ee2e524fe16
Ярослав Розлуцкий <papa7717@mail.ru>
2022-08-31 20:34:16 +0300

    Мелкие изменения

--- 10thHW/Weather/model.py
+++ 10thHW/Weather/model.py
@@ -1,12 +1,10 @@
 from pyowm import OWM
 from pyowm.utils import config
 from pyowm.utils import timestamps
- 
-owm = OWM('6eb60b5fc9bbb98369ba61684926f218')
-mgr = owm.weather_manager()
- 
- 
-observation = mgr.weather_at_place('London,GB')
-w = observation.weather
- 
-print(w.temperature('celsius'))  # {'temp_max': 10.5, 'temp': 9.7, 'temp_min': 9.0}
+
+def GetWeather(): 
+    owm = OWM('OWN API key')   
+    mngr = owm.weather_manager()
+    observation = mngr.weather_at_place('Saint Petersburg, RU')
+    w = observation.weather
+    print(w.temperature('celsius'))
